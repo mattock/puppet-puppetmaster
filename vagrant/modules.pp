b2ec43347f34ad362cfc70d8a8d34f9af926a2fa
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2018-11-23 16:28:47 +0200

    Always log output of librarian-puppet
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- vagrant/modules.pp
+++ vagrant/modules.pp
@@ -5,6 +5,7 @@ exec { 'Run librarian-puppet':
   logoutput => true,
   command   => 'librarian-puppet install --verbose',
   timeout   => 600,
+  logoutput => true,
   path      => ['/bin','/usr/bin','/opt/puppetlabs/bin','/opt/puppetlabs/puppet/bin'],
 }
 
